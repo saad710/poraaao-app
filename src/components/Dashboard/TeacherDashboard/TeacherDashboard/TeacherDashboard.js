1cb468450f58cb42ce1a9c65314ddbbd9a8598a2
SakifKhan98 <sakifkhan98@gmail.com>
2020-11-15 03:06:06 +0600

    Dashboard Footer Added

--- src/components/Dashboard/TeacherDashboard/TeacherDashboard/TeacherDashboard.js
+++ src/components/Dashboard/TeacherDashboard/TeacherDashboard/TeacherDashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../../Shared/Footer/Footer";
 import TeacherContent from "../TeacherContent/TeacherContent";
 import TeacherNavbar from "../TeacherNavbar/TeacherNavbar";
 import TeacherSidebar from "../TeacherSidebar/TeacherSidebar";
@@ -6,10 +7,13 @@ import TeacherSidebar from "../TeacherSidebar/TeacherSidebar";
 const TeacherDashboard = () => {
   return (
     <div>
-      <TeacherNavbar />
+      <TeacherNavbar></TeacherNavbar>
       <div style={{ display: "flex", alignItems: "space-around" }}>
-        <TeacherSidebar />
-        <TeacherContent />
+        <TeacherSidebar></TeacherSidebar>
+        <TeacherContent></TeacherContent>
+      </div>
+      <div style={{ backgroundColor: "#212121" }}>
+        <Footer></Footer>
       </div>
     </div>
   );
